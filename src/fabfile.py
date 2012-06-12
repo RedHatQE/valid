6a6c02184adfdc8fd011316f2a607f279d941fdf
milan <mkovacik@redhat.com>
2012-06-12 12:26:27 +0200

    fabfile

--- src/fabfile.py
+++ src/fabfile.py
@@ -4,12 +4,7 @@ from fabric.api import env, task, run, settings, execute
 env.key_filename = ['/home/mkovacik/.pem/aws-us-east-1.pem']
 env.user = 'root'
 
-key_name = 'aws-us-east-1'
-ami = 'ami-41d00528'
-instance_type = 't1.micro' #{"name":"t1.micro","memory":"600000","cpu":"1","arch":"x86_64"}
 
-ec2_key='AKIAJI4XN4LZAHCZUUAQ'
-ec2_secret_key='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'
 
 env.timeout = 5
 env.connection_attempts = 12
