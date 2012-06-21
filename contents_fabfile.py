67c392b068eed2e9eca81cac38f3994902a1a45a
milan <mkovacik@redhat.com>
2012-06-21 10:58:33 +0200

    update before contents test

--- contents_fabfile.py
+++ contents_fabfile.py
@@ -1,4 +1,5 @@
-from fabric.api import run, local, task, env
+from fabric.api import run, local, task, env, execute
+from fabric.operations import reboot
 from pprint import pprint
 import urllib, json
 
@@ -10,11 +11,38 @@ def manifest_cdn_package_set(manifest):
 			packages += manifest['cdn']['products'][product]['Repo Paths'][path]
 	return set(packages)
 
+def to_bool(str):
+	ret = False
+	ret |= str == 'y'
+	ret |= str == 'Y'
+	ret |= str == 'Yes'
+	ret |= str == 'yes'
+	ret |= str == 'True'
+	ret |= str == 'true'
+	ret |= str == '1'
+	return ret
+
 @task
-def contents_check(manifest):
+def prepare_staging():
+	"""modify system to work with stage RHUI"""
+	run('echo "10.3.94.100 rhui2-cds01-stage.us-east-1.aws.ce.redhat.com" >> /etc/hosts')
+	run("sed -i 's/rhui2-cds01.us-east-1.aws.ce.redhat.com/rhui2-cds01-stage.us-east-1.aws.ce.redhat.com/' /etc/yum.repos.d/redhat-rhui.repo")
+	run('echo rhui2-cds01-stage.us-east-1.aws.ce.redhat.com > /etc/yum.repos.d/rhui-load-balancers.conf')
+
+@task
+def update(stage=False):
+	"""perform system update"""
+	if stage:
+		execute(prepare_staging)
+	run('/usr/bin/yum update -y')
+	reboot(wait=180)
+
+@task
+def contents_check(manifest, stage=False):
 	"""print packages not provided by given CDN manifest URL"""
 	manifest_j = json.load(urllib.urlopen(manifest))
 	packages = manifest_cdn_package_set(manifest_j)
+	execute(update, stage=to_bool(stage))
 	installed_packages = set(run("/bin/rpm -qa | sed -e 's/$/.rpm/'").split('\r\n'))
 	print "# --\n# %s" % (env.host_string,)
 	pprint (installed_packages - packages)
