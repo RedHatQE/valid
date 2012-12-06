e24a6df57eb9bed5f9c7b1953191c120ccbcbdf7
Vitaly Kuznetsov <vitty@redhat.com>
2012-12-06 17:27:32 +0100

    itestcase_04_cloud_firstboot.py introduced

--- valid/testing_modules/__init__.py
+++ valid/testing_modules/__init__.py
@@ -1,4 +1,5 @@
 import testcase_01_ipv6
 import testcase_02_bash_history
 import testcase_03_chkconfig
+import testcase_04_cloud_firstboot
 import testcase_50_auditd
---
+++ valid/testing_modules/testcase_04_cloud_firstboot.py
@@ -0,0 +1,11 @@
+from valid.valid_testcase import *
+
+class testcase_04_cloud_firstboot(ValidTestcase):
+    def test(self, connection, params):
+        if (params["product"].upper() == "RHEL" or params["product"].upper() == "BETA") and params["version"].startswith("6.0"):
+            self.log.append({"result": "passed", "comment": "waived test for bugzilla 704821"})
+        else:
+            self.ping_pong(connection, "chkconfig --list rh-cloud-firstboot", "3:off")
+            self.ping_pong(connection, "test -f /etc/sysconfig/rh-cloud-firstboot && echo SUCCESS", "[^ ]SUCCESS")
+            self.ping_pong(connection, "cat /etc/sysconfig/rh-cloud-firstboot", "RUN_FIRSTBOOT=NO")
+        return self.log
