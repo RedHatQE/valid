823b52edc6196df80a0ae9ada24f5543fef3394f
Vitaly Kuznetsov <vitty@redhat.com>
2012-12-07 16:44:21 +0100

    testcase_21_disk_size_format implemented

--- valid/testing_modules/__init__.py
+++ valid/testing_modules/__init__.py
@@ -18,3 +18,4 @@ import testcase_17_shells
 import testcase_18_sshd
 import testcase_19_rhn_system_id
 import testcase_20_auditd
+import testcase_21_disk_size_format
---
+++ valid/testing_modules/testcase_21_disk_size_format.py
@@ -0,0 +1,17 @@
+from valid.valid_testcase import *
+
+class testcase_21_disk_size_format(ValidTestcase):
+    def test(self, connection, params):
+
+        disks = self.match(connection,  "echo '###' ;mount | grep '^/dev' | awk '{print $1}'; echo '###'", re.compile(".*\r\n###\r\n(.*)\r\n###\r\n.*", re.DOTALL))
+        if disks:
+            mpoint = self.match(connection,  "echo '###' ;mount | grep '^%s' | awk '{print $3}'; echo '###'" % disks[0], re.compile(".*\r\n###\r\n(.*)\r\n###\r\n.*", re.DOTALL))
+            fs = self.match(connection,  "echo '###' ;mount | grep '^%s' | awk '{print $5}'; echo '###'" % disks[0], re.compile(".*\r\n###\r\n(.*)\r\n###\r\n.*", re.DOTALL))
+            if mpoint and fs:
+                for disk in disks[0].split():
+                    self.ping_pong(connection, "[ `df -k %s | awk '{ print $2 }' | tail -n 1` -gt 3937219 ] && echo SUCCESS" % disk, "\r\nSUCCESS\r\n")
+                    if mpoint[0]=='/' and ((params["product"].upper() == "RHEL" or params["product"].upper() == "BETA") and params["version"].startswith("6.")):
+                        self.ping_pong(connection, "[ %s = ext4 ] && echo SUCCESS"  % fs[0], "\r\nSUCCESS\r\n")
+                    else:
+                        self.ping_pong(connection, "[ %s = ext3 ] && echo SUCCESS" % fs[0], "\r\nSUCCESS\r\n")
+        return self.log
