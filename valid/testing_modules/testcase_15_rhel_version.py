8a06b538754e0bd83aec83a398e94342dffe9128
Vitaly Kuznetsov <vitty@redhat.com>
2012-12-07 15:36:53 +0100

    testcase_15_rhel_version implemented

---
+++ valid/testing_modules/testcase_15_rhel_version.py
@@ -0,0 +1,11 @@
+from valid.valid_testcase import *
+
+class testcase_15_rhel_version(ValidTestcase):
+    def test(self, connection, params):
+        if (params["product"].upper() == "RHEL" or params["product"].upper() == "BETA"):
+            rhelv = self.match(connection, "rpm -q --qf '%{RELEASE}\n' --whatprovides redhat-release", re.compile(".*\r\n([0-9]\.[0-9]\..*)\r\n.*", re.DOTALL))
+            if rhelv:
+                self.ping_pong(connection, "[ '%s' = '%s' ] && echo SUCCESS" % (params["version"], rhelv[0][:len(params["version"])]), "[^ ]SUCCESS")
+        else:
+            self.log.append({"result": "failure", "comment": "this test is for RHEL only"})
+        return self.log
