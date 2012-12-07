b2ece943a6037d1bd6924305d66f4cd7e6f9bee6
Vitaly Kuznetsov <vitty@redhat.com>
2012-12-07 16:02:13 +0100

    testcase_17_shells implemented

--- valid/testing_modules/__init__.py
+++ valid/testing_modules/__init__.py
@@ -14,4 +14,5 @@ import testcase_13_resize2fs
 import testcase_14_host_details
 import testcase_15_rhel_version
 import testcase_16_selinux
+import testcase_17_shells
 import testcase_50_auditd
---
+++ valid/testing_modules/testcase_17_shells.py
@@ -0,0 +1,7 @@
+from valid.valid_testcase import *
+
+class testcase_17_shells(ValidTestcase):
+    def test(self, connection, params):
+        self.ping_pong(connection, "grep 'bin/bash$' /etc/shells && echo SUCCESS", "\r\nSUCCESS\r\n")
+        self.ping_pong(connection, "grep 'bin/nologin$' /etc/shells && echo SUCCESS", "\r\nSUCCESS\r\n")
+        return self.log
