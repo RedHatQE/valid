8ff98615889da0e995e993d69391ff1858491b6d
Vitaly Kuznetsov <vitty@redhat.com>
2012-12-10 09:56:04 +0100

    testcase_23_syslog.py: hotfixes

--- valid/testing_modules/testcase_23_syslog.py
+++ valid/testing_modules/testcase_23_syslog.py
@@ -2,14 +2,16 @@ from valid.valid_testcase import *
 
 class testcase_23_syslog(ValidTestcase):
     def test(self, connection, params):
+        prod = params["product"].upper()
+        ver = params["version"]
         rsyslog_md5 = self.get_result(connection, "md5sum /etc/rsyslog.conf | cut -f 1 -d ' '")
         if rsyslog_md5:
-            if (params["product"].upper() == "RHEL" or params["product"].upper() == "BETA") and params["version"].startswith("5."):
-                self.ping_pong(connection, "([ %s = bd4e328df4b59d41979ef7202a05e074 ] || [ %s = 15936b6fe4e8fadcea87b54de495f975 ]) && echo SUCCESS", "\r\nSUCCESS\r\n)")
-            elif (params["product"].upper() == "RHEL" or params["product"].upper() == "BETA") and (params["version"].startswith("6.0") or params["version"].startswith("6.1") or params["version"].startswith("6.2")):
-                self.ping_pong(connection, "[ %s = dd356958ca9c4e779f7fac13dde3c1b5 ] && echo SUCCESS", "\r\nSUCCESS\r\n)")
-            elif (params["product"].upper() == "RHEL" or params["product"].upper() == "BETA") and params["version"].startswith("6."):
-                self.ping_pong(connection, "[ %s = 8b91b32300134e98ef4aee632ed61e21 ] && echo SUCCESS", "\r\nSUCCESS\r\n)")
+            if prod in ["RHEL", "BETA"] and ver.startswith("5."):
+                self.ping_pong(connection, "([ %s = bd4e328df4b59d41979ef7202a05e074 ] || [ %s = 15936b6fe4e8fadcea87b54de495f975 ]) && echo SUCCESS" % (rsyslog_md5, rsyslog_md5), "\r\nSUCCESS\r\n")
+            elif prod in ["RHEL", "BETA"] and ver[:3] in ["6.0", "6.1", "6.2"]:
+                self.ping_pong(connection, "[ %s = dd356958ca9c4e779f7fac13dde3c1b5 ] && echo SUCCESS" % rsyslog_md5, "\r\nSUCCESS\r\n")
+            elif prod in ["RHEL", "BETA"] and ver.startswith("6."):
+                self.ping_pong(connection, "[ %s = 8b91b32300134e98ef4aee632ed61e21 ] && echo SUCCESS" % rsyslog_md5, "\r\nSUCCESS\r\n")
             else:
                 self.log.append({"result": "failure", "comment": "this test is for RHEL5/RHEL6 only"})
         return self.log
