effaacfd72509c8d14327b43603494f31b3b5373
Vitaly Kuznetsov <vitty@redhat.com>
2012-12-10 17:55:16 +0100

    testcase_30_yum_package_install: increase timeouts

--- valid/testing_modules/testcase_30_yum_package_install.py
+++ valid/testing_modules/testcase_30_yum_package_install.py
@@ -4,9 +4,9 @@ class testcase_30_yum_package_install(ValidTestcase):
     stages = ["stage1"]
 
     def test(self, connection, params):
-        self.ping_pong(connection, "yum repolist && echo SUCCESS", "\r\nSUCCESS\r\n", 60)
-        self.ping_pong(connection, "yum search zsh && echo SUCCESS", "\r\nSUCCESS\r\n", 60)
-        self.ping_pong(connection, "yum -y install zsh && echo SUCCESS", "\r\nSUCCESS\r\n", 90)
-        self.ping_pong(connection, "rpm -q --queryformat '%{NAME}\n' zsh", "\r\nzsh\r\n", 20)
-        self.ping_pong(connection, "rpm -e zsh && echo SUCCESS", "\r\nSUCCESS\r\n", 30)
+        self.ping_pong(connection, "yum repolist && echo SUCCESS", "\r\nSUCCESS\r\n", 120)
+        self.ping_pong(connection, "yum search zsh && echo SUCCESS", "\r\nSUCCESS\r\n", 120)
+        self.ping_pong(connection, "yum -y install zsh && echo SUCCESS", "\r\nSUCCESS\r\n", 180)
+        self.ping_pong(connection, "rpm -q --queryformat '%{NAME}' zsh && echo", "\r\nzsh\r\n", 30)
+        self.ping_pong(connection, "rpm -e zsh && echo SUCCESS", "\r\nSUCCESS\r\n", 60)
         return self.log
