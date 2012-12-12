0ce46d484a6248689ea671f6d09a261126b0df30
Vitaly Kuznetsov <vitty@redhat.com>
2012-12-12 17:50:40 +0100

    testcase_99_reboot.py: workaround several issues

--- valid/testing_modules/testcase_99_reboot.py
+++ valid/testing_modules/testcase_99_reboot.py
@@ -6,6 +6,14 @@ class testcase_99_reboot(ValidTestcase):
     stages = ["stage1"]
 
     def test(self, connection, params):
+        if prod in ["RHEL", "BETA"] and ver.startswith("6.") and params["hwp"]["name"]=="t1.micro":
+            # Creating swap to workaround mem<1024M issue
+            self.get_return_value(connection, "head -c $((1024*1024*1024)) /dev/zero > /swap", 120)
+            self.get_return_value(connection, "mkswap /swap", 30)
+            self.get_return_value(connection, "echo '/swap    swap     swap    defaults     0 0' >> /etc/fstab")
+        elif prod in ["RHEL", "BETA"] and ver.startswith("5."):
+            # Booting the latest kernel for stage2 testing
+            self.get_return_value(connection, "sed -i 's,\(default\)=.*$,\1=0,' /boot/grub/menu.lst")
         try:
             self.get_return_value(connection, "reboot")
         except (paramiko.SSHException, EOFError), e:
