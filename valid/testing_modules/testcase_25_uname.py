29b952e899ca5beebc0055a664a6320a97411ed2
Vitaly Kuznetsov <vitty@redhat.com>
2012-12-13 13:40:10 +0100

    testcase_25_uname.py: fix for RHEL5

--- valid/testing_modules/testcase_25_uname.py
+++ valid/testing_modules/testcase_25_uname.py
@@ -7,12 +7,12 @@ class testcase_25_uname(ValidTestcase):
         prod = params["product"].upper()
         ver = params["version"]
         if prod in ["RHEL", "BETA"] and ver.startswith("5."):
-            pkgname = "kernel-xen"
+            uname_r = self.get_result(connection, "uname -r | sed 's,\.el5xen,.el5,'")
+            kernel_ver = self.get_result(connection, "rpm -q --queryformat '%{VERSION}-%{RELEASE}\n' kernel-xen | sort | tail -1")
         else:
-            pkgname = "kernel"
-        uname_r = self.get_result(connection, "uname -r")
+            uname_r = self.get_result(connection, "uname -r")
+            kernel_ver = self.get_result(connection, "rpm -q --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel | sort | tail -1")
         uname_o = self.get_result(connection, "uname -o")
-        kernel_ver = self.get_result(connection, "rpm -q --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}\n' " + pkgname + " | sort | tail -1")
         if uname_r and uname_o and kernel_ver:
             self.get_return_value(connection, "[ %s = %s ]" % (kernel_ver, uname_r))
             self.get_return_value(connection, "[ %s = 'GNU/Linux' ]" % uname_o)
