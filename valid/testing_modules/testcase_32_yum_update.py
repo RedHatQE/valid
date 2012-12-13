390b6ca5962259ba42797321685a7ff5d7d4ff0e
Vitaly Kuznetsov <vitty@redhat.com>
2012-12-13 10:46:50 +0100

    Create swap for t1.micro in yum_update (to avoid OOM)

--- valid/testing_modules/testcase_32_yum_update.py
+++ valid/testing_modules/testcase_32_yum_update.py
@@ -4,5 +4,13 @@ class testcase_32_yum_update(ValidTestcase):
     stages = ["stage1"]
 
     def test(self, connection, params):
+        prod = params["product"].upper()
+        ver = params["version"]
+        if prod in ["RHEL", "BETA"] and ver.startswith("6.") and params["hwp"]["name"]=="t1.micro":
+            # Creating swap to workaround mem<1024M issue
+            self.ping_pong(connection, "head -c $((1024*1024*1024)) /dev/zero > /swap && echo SUCCESS", "\r\nSUCCESS\r\n", 150)
+            self.get_return_value(connection, "mkswap /swap", 30)
+            self.ping_pong(connection, "echo '/swap    swap     swap    defaults     0 0' >> /etc/fstab && echo SUCCESS", "\r\nSUCCESS\r\n")
+            self.get_return_value(connection, "swapon -a", 30)
         self.get_return_value(connection, "yum -y update", 900)
         return self.log
--- valid/testing_modules/testcase_99_reboot.py
+++ valid/testing_modules/testcase_99_reboot.py
@@ -8,12 +8,7 @@ class testcase_99_reboot(ValidTestcase):
     def test(self, connection, params):
         prod = params["product"].upper()
         ver = params["version"]
-        if prod in ["RHEL", "BETA"] and ver.startswith("6.") and params["hwp"]["name"]=="t1.micro":
-            # Creating swap to workaround mem<1024M issue
-            self.get_return_value(connection, "head -c $((1024*1024*1024)) /dev/zero > /swap", 120)
-            self.get_return_value(connection, "mkswap /swap", 30)
-            self.get_return_value(connection, "echo '/swap    swap     swap    defaults     0 0' >> /etc/fstab")
-        elif prod in ["RHEL", "BETA"] and ver.startswith("5."):
+        if prod in ["RHEL", "BETA"] and ver.startswith("5."):
             # Booting the latest kernel for stage2 testing
             self.get_return_value(connection, "sed -i 's,\(default\)=.*$,\1=0,' /boot/grub/menu.lst")
         try:
