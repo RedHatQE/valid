ce30a92db4bc03593a8d426130722c89741816aa
Vitaly Kuznetsov <vitty@redhat.com>
2012-12-17 14:19:00 +0100

    Fix testcase_14_host_details.py to run for non-RHEL products

--- valid/testing_modules/testcase_14_host_details.py
+++ valid/testing_modules/testcase_14_host_details.py
@@ -5,18 +5,24 @@ class testcase_14_host_details(ValidTestcase):
     stages = ["stage1"]
 
     def test(self, connection, params):
+        prod = params["product"].upper()
         self.get_return_value(connection, '[ ! -z "`curl http://169.254.169.254/latest/dynamic/instance-identity/signature`" ]')
         json_str = self.match(connection, "curl http://169.254.169.254/latest/dynamic/instance-identity/document", re.compile(".*({.*}).*", re.DOTALL))
         if json_str:
             try:
                 js = json.loads(json_str[0])
+                if "billingProducts" in js.keys() and js["billingProducts"]!=None:
+                    billingProduct = js["billingProducts"][0]
+                else:
+                    billingProduct = ""
                 self.get_return_value(connection, '[ "%s" = "%s" ]' % (js["imageId"], params["ami"]))
                 self.get_return_value(connection, '[ "%s" = "%s" ]' % (js["architecture"], params["hwp"]["arch"]))
                 self.get_return_value(connection, '[ "%s" = "%s" ]' % (js["region"], params["region"]))
-                if params["itype"] == "hourly":
-                    self.get_return_value(connection, '[ "%s" = "%s" ]' % (js["billingProducts"][0], "bp-6fa54006"))
-                elif params["itype"] == "access":
-                    self.get_return_value(connection, '[ "%s" = "%s" ]' % (js["billingProducts"][0], "bp-63a5400a"))
+                if prod in ["RHEL", "BETA"]:
+                    if params["itype"] == "hourly":
+                        self.get_return_value(connection, '[ "%s" = "%s" ]' % (billingProduct, "bp-6fa54006"))
+                    elif params["itype"] == "access":
+                        self.get_return_value(connection, '[ "%s" = "%s" ]' % (billingProduct, "bp-63a5400a"))
             except KeyError:
                 self.log.append({"result": "failure", "comment": "failed to check instance details, " + e.message})
         else:
