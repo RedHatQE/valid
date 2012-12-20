e66397210d576cf5272e0cdf5e721780df7410e1
milan <mkovacik@redhat.com>
2012-12-20 12:12:22 +0100

    fixed the reading

--- valid/testing_modules/testcase_27_yum_repos.py
+++ valid/testing_modules/testcase_27_yum_repos.py
@@ -18,11 +18,16 @@ class testcase_27_yum_repos(ValidTestcase):
             40
         )
         # translate the details into an ini-like structure
-        repos_details = self.get_result(
-           connection,
-           "head -n-1 repolist_xx00 | sed -e 's/Repo-id\s*:\s*\(.*\)/[\1]/'"
-        )
+        repos_fp = connection.sftp.open('/root/repolist_xx00')
+        repos_details = repos_fp.read()
+        repos_fp.close()
+
+        # make 'Repo-id : <id>' ini section headers: '[<id>]'
+        pattern = re.compile('repo-id\s*:\s*([^\n]*)', re.DOTALL | re.IGNORECASE)
+        repos_details = pattern.sub('[\\1]', repos_details)
         # extract particular repos as sections from the structure
+        # please note that ConfigParser makes all strings lower case by
+        # default
         repos_fp = StringIO.StringIO(repos_details)
         repos_conf = ConfigParser.ConfigParser()
         repos_conf.readfp(repos_fp)
@@ -41,8 +46,8 @@ class testcase_27_yum_repos(ValidTestcase):
             all_repos = yaml.safe_load(expected_repos_fd)
         expected_repos = all_repos[params['region']]['%s-%s' % (prod, ver[0])]
         ret = {
-            "comment": "# expected repos:\n%s\n# actual repos:\n%s" %
-                (yaml.dump(expected_repos), yaml.dump(repos))
+            "expected repos": expected_repos,
+            "actual repos": repos
         }
         ret['result'] = expected_repos == repos and 'passed' or 'failed'
         self.log.append(ret)
