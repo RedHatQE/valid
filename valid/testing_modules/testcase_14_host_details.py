d90501a0e8f0822dda81ba5d1c4aa676cd4dfdac
milan <mkovacik@redhat.com>
2013-01-02 21:29:18 +0100

    fixed key error handling

--- valid/testing_modules/testcase_14_host_details.py
+++ valid/testing_modules/testcase_14_host_details.py
@@ -24,7 +24,7 @@ class testcase_14_host_details(ValidTestcase):
                         self.get_return_value(connection, '[ "%s" = "%s" ]' % (billingProduct, "bp-6fa54006"))
                     elif params["itype"] == "access":
                         self.get_return_value(connection, '[ "%s" = "%s" ]' % (billingProduct, "bp-63a5400a"))
-            except KeyError:
+            except KeyErrori as e:
                 self.log.append({"result": "failure", "comment": "failed to check instance details, " + e.message})
         else:
             self.log.append({"result": "failure", "comment": "failed to get instance details"})
