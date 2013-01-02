d00988390115765f7a52825b077dfd4be8d9f0d6
Vitaly Kuznetsov <vitty@redhat.com>
2013-01-02 14:18:07 +0100

    testcase_08_memory.py: fix for colorful output

--- valid/testing_modules/testcase_08_memory.py
+++ valid/testing_modules/testcase_08_memory.py
@@ -9,7 +9,7 @@ class testcase_08_memory(ValidTestcase):
         if not "memory" in params.keys():
             self.log.append({"result": "failure", "comment": "memory parameter is not set"})
         else:
-            values = self.match(connection, "grep 'MemTotal:' /proc/meminfo", re.compile(".*\r\nMemTotal:\s*([0-9]+) ", re.DOTALL))
+            values = self.match(connection, "grep --color=none 'MemTotal:' /proc/meminfo", re.compile(".*\r\nMemTotal:\s*([0-9]+) ", re.DOTALL))
             if values:
                 self.get_return_value(connection, "[ %s -gt %s ]" % (values[0], params["memory"]))
         return self.log
