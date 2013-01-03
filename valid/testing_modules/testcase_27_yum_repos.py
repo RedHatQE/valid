b2ef6db8e459938e70872b34f814916dcf0e9efa
milan <mkovacik@redhat.com>
2013-01-03 18:15:38 +0100

    items not collected

--- valid/testing_modules/testcase_27_yum_repos.py
+++ valid/testing_modules/testcase_27_yum_repos.py
@@ -62,7 +62,7 @@ class testcase_27_yum_repos(ValidTestcase):
             return self.log
         # add the region substring
         expected_repos = {}
-        for k, v in expected_repos_:
+        for k, v in expected_repos_.items():
             expected_repos[k.replace('rhui', '-' + params['region'])] = v
         ret = {
             "expected repos": expected_repos,
