bfe332eb5c6b2850711bca31dd06b37c08958230
Vitaly Kuznetsov <vitty@redhat.com>
2013-01-04 13:46:41 +0100

    testcase_34_cpu.py introduced

--- valid/testing_modules/__init__.py
+++ valid/testing_modules/__init__.py
@@ -31,6 +31,7 @@ import testcase_30_rhn_certificates
 import testcase_31_subscription_management
 import testcase_32_ephemeral
 import testcase_33_userdata
+import testcase_34_cpu
 import testcase_50_yum_package_install
 import testcase_55_yum_group_install
 import testcase_60_yum_update
