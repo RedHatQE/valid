21be4162a03cc77266ab89033f5622eaf25423b8
milan <mkovacik@redhat.com>
2013-01-15 13:21:49 +0100

    cloud-init adjustments

--- valid/testing_modules/testcase_13_resize2fs.py
+++ valid/testing_modules/testcase_13_resize2fs.py
@@ -6,10 +6,15 @@ class testcase_13_resize2fs(ValidTestcase):
     applicable = {"product": "(?i)RHEL|BETA", "version": "5.*|6.*", "virtualization": "(?!hvm)"}
 
     def test(self, connection, params):
-        if (params["product"].upper() == "RHEL" or params["product"].upper() == "BETA") and params["version"].startswith("6."):
-            self.get_return_value(connection, "if [ -b /dev/xvde1 ]; then resize2fs -p /dev/xvde1 15000M ; else resize2fs -p /dev/xvda1 15000M; fi", 90)
-            self.get_return_value(connection, "df -h | grep 15G")
-        elif (params["product"].upper() == "RHEL" or params["product"].upper() == "BETA") and params["version"].startswith("5."):
-            self.get_return_value(connection, "resize2fs -p /dev/sda1 15000M", 90)
-            self.get_return_value(connection, "df -h | grep 15G")
+        """The instances are always created with a 15GB root device (unlike
+        the AWS default of 8GB. The point is whether we're able to allocate
+        the space. Please note that cloud-init does the resize automatically.
+        """
+        if self.get_return_value(connection, 'rpm -q cloud-init', nolog=True) == 1:
+            # cloud-init not installed, resize
+            if (params["product"].upper() == "RHEL" or params["product"].upper() == "BETA") and params["version"].startswith("6."):
+                self.get_return_value(connection, "if [ -b /dev/xvde1 ]; then resize2fs -p /dev/xvde1 15000M ; else resize2fs -p /dev/xvda1 15000M; fi", 90)
+            elif (params["product"].upper() == "RHEL" or params["product"].upper() == "BETA") and params["version"].startswith("5."):
+                self.get_return_value(connection, "resize2fs -p /dev/sda1 15000M", 90)
+        self.get_return_value(connection, "df -h | grep 15G")
         return self.log
