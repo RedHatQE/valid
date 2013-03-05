909e97344311d1ed62fb74ed54244a27eca5aca3
Vitaly Kuznetsov <vitty@redhat.com>
2013-03-05 16:31:15 +0100

    valid_result.py library introduced

--- scripts/valid_bugzilla_reporter.py
+++ scripts/valid_bugzilla_reporter.py
@@ -6,7 +6,7 @@ import yaml
 import bugzilla
 import tempfile
 import sys
-
+from valid import valid_result
 
 class Summary(object):
     def __init__(self, contents = [], verbose=False):
@@ -57,52 +57,10 @@ if not bzid:
     sys.exit(1)
 
 for ami in result:
-    arch = ami["arch"]
-    product = ami["product"]
-    region = ami["region"]
-    version = ami["version"]
-    ami_result = ami["result"]
-    overall_result = "succeeded"
-    bug_summary = ami["ami"] + " " + product + " " + version + " " + arch + " " + region
-    bug_description = ""
-
     ami_fd = tempfile.NamedTemporaryFile()
     ami_fd.write(yaml.safe_dump(ami))
     ami_fd.seek(0)
-
-    for itype in ami_result.keys():
-        bug_description += itype + "\n"
-        itype_result = ami_result[itype]
-        if type(itype_result) == dict:
-            for stage in itype_result.keys():
-                bug_description += stage + ":\n"
-                stage_result = itype_result[stage]
-                if type(stage_result) == dict:
-                    for test in sorted(stage_result.keys()):
-                        test_result = stage_result[test]
-                        if type(test_result) == list:
-                            is_failed = "succeeded"
-                            for command in test_result:
-                                if command["result"] in ["fail", "failed", "failure"]:
-                                    is_failed = "failed"
-                                    if overall_result == "succeeded":
-                                        overall_result = "failed"
-                                if command["result"] in ["skip", "skipped"]:
-                                    is_failed = "skipped"
-                            bug_description += "test %s %s\n" % (test, is_failed)
-                            if is_failed != "succeeded":
-                                for command in test_result:
-                                    bug_description += "--->\n"
-                                    for key in sorted(command.keys()):
-                                        bug_description += "\t%s: %s\n" % (key, command[key])
-                                    bug_description += "<---\n"
-                else:
-                    bug_description += "stage testing failed!\n"
-                    overall_result = "failure"
-        else:
-            bug_description += "instance testing failed!\n"
-            overall_result = "failure"
-    bug_description = "Validation " + overall_result + " for " + ami["ami"] + " in " + region + " product: " + product + ", version: " + version + ", arch: " + arch + "\n\n" + bug_description
+    overall_result, bug_summary, bug_description = valid_result.get_overall_result(ami)
 
     if args.test:
         summary.add(ami['ami'], status='fail')
