09aa901e55b26ed707fed308ae028666abc0b7ab
Vitaly Kuznetsov <vitty@redhat.com>
2013-03-07 16:12:14 +0100

    testcase_98_kernel_upgrade_pre.py: provide 'update from repo' feature

--- valid/testing_modules/testcase_98_kernel_upgrade_pre.py
+++ valid/testing_modules/testcase_98_kernel_upgrade_pre.py
@@ -5,24 +5,31 @@ from valid.valid_testcase import *
 
 
 class testcase_98_kernel_upgrade_pre(ValidTestcase):
+    tags = ["kernel"]
     stages = ["stage0"]
 
     def test(self, connection, params):
-        if not "kernelpkg" in params:
+        if "kernelpkg" in params:
+            kernelfile = params["kernelpkg"]
+            kernelbase = os.path.basename(kernelfile)
+            connection.sftp.put(kernelfile,"/tmp/%s" % kernelbase)
+            self.get_return_value(connection, "ls -l /tmp/%s" % kernelbase)
+            self.get_return_value(connection, "yum -y install /tmp/%s" % kernelbase, 300)
+        else:
+            # doing upgrade from repo
+            self.get_return_value(connection, "yum -y install kernel", 300)
+        kernel_updated = self.get_return_value(connection, "[ $(rpm -qa kernel | wc -l) -gt 1 ]", nolog=True)
+        if kernel_updated == 0:
+            # removing old kernel - no way to boot it on EC2 anyway :-)
+            self.get_return_value(connection, "rpm -e kernel-`uname -r`", 30)
+            try:
+                self.get_return_value(connection, "reboot", nolog=True)
+            except (paramiko.SSHException, EOFError), e:
+                self.log.append({"result": "passed", "command": "reboot"})
+            time.sleep(30)
+        else:
             self.log.append({
                     "result": "skip",
-                    "comment": "No kernel package provided for upgrade"
+                    "comment": "no kernel upgrade was done"
                     })
-            return self.log
-        kernelfile = params["kernelpkg"]
-        kernelbase = os.path.basename(kernelfile)
-        connection.sftp.put(kernelfile,"/tmp/%s" % kernelbase)
-        self.get_return_value(connection, "ls -l /tmp/%s" % kernelbase)
-        self.get_return_value(connection, "yum -y install /tmp/%s" % kernelbase, 300)
-        self.get_return_value(connection, "rpm -e kernel-`uname -r`", 30)
-        try:
-            self.get_return_value(connection, "reboot", nolog=True)
-        except (paramiko.SSHException, EOFError), e:
-            self.log.append({"result": "passed", "command": "reboot"})
-        time.sleep(30)
         return self.log
