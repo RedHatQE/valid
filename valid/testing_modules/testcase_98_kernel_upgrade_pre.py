1643ae9f7c02f706110e3f32df5d06100e42d713
Vitaly Kuznetsov <vitty@redhat.com>
2013-04-08 11:39:21 +0200

    testcase_98_kernel_upgrade_pre.py: install kernels with rpm

--- valid/testing_modules/testcase_98_kernel_upgrade_pre.py
+++ valid/testing_modules/testcase_98_kernel_upgrade_pre.py
@@ -26,7 +26,7 @@ class testcase_98_kernel_upgrade_pre(ValidTestcase):
                 connection.sftp.put(pkg, '/tmp/%s' % pkgbase)
                 kernelfiles += '/tmp/%s ' % pkgbase
                 self.get_return_value(connection, 'ls -l /tmp/%s' % pkgbase)
-            self.get_return_value(connection, 'yum -y install %s' % kernelfiles, 300)
+            self.get_return_value(connection, 'rpm -i %s' % kernelfiles, 300)
         else:
             # doing upgrade from repo
             self.get_return_value(connection, 'yum -y install kernel', 300)
