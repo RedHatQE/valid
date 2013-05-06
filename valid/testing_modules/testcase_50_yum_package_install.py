510954a97509238044932cb6035bf0e0f8c1acd4
Vitaly Kuznetsov <vitty@redhat.com>
2013-05-06 14:12:56 +0200

    testcase_50_yum_package_install: add 'yum check-update' check

--- valid/testing_modules/testcase_50_yum_package_install.py
+++ valid/testing_modules/testcase_50_yum_package_install.py
@@ -10,7 +10,12 @@ class testcase_50_yum_package_install(ValidTestcase):
     tags = ['default']
 
     def test(self, connection, params):
+        self.get_return_value(connection, 'yum clean all', 30)
         self.get_return_value(connection, 'yum repolist', 120)
+        checkupdate = self.get_return_value(connection, 'yum check-update', 120, nolog=True)
+        if not checkupdate in [0, 100]:
+            # 100 means 'we have an update'
+            self.log.append({"result": "failed", "command": "yum check-update", "actual": str(checkupdate)})
         self.get_return_value(connection, 'yum search zsh', 120)
         self.get_return_value(connection, 'yum -y install zsh', 180)
         self.get_return_value(connection, 'rpm -q --queryformat \'%{NAME}\' zsh', 30)
