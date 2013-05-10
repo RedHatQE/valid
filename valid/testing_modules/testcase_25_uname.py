4068ae7b655c10d04f6200166b3eb26ca11d8b34
Vitaly Kuznetsov <vitty@redhat.com>
2013-05-10 23:07:39 +0200

    testcase_25_uname.py: sort kernels right

--- valid/testing_modules/testcase_25_uname.py
+++ valid/testing_modules/testcase_25_uname.py
@@ -21,7 +21,7 @@ class testcase_25_uname(ValidTestcase):
         if prod in ['RHEL', 'BETA']:
             if ver.startswith('5.'):
                 uname_r = self.get_result(connection, 'uname -r | sed \'s,\.el5xen,.el5,\'')
-                kernel_ver = self.get_result(connection, 'rpm -q --queryformat \'%{VERSION}-%{RELEASE}\n\' kernel-xen | sort | tail -1', 30)
+                kernel_ver = self.get_result(connection, 'rpm -q --last kernel-xen | sed -e \'s,^kernel-xen-,,\' -e \'s,[[:space:]].*$,,\' | head -1', 30)
             else:
                 uname_r = self.get_result(connection, 'uname -r')
                 kernel_ver = self.get_result(connection, 'rpm -q --last kernel | sed -e \'s,^kernel-,,\' -e \'s,[[:space:]].*$,,\' | head -1', 30)
