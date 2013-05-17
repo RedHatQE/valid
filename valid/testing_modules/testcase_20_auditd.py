6ae28c09600f2ada819e989e228a60e8af5aa6ad
Vitaly Kuznetsov <vitty@redhat.com>
2013-05-17 12:06:12 +0200

    testcase_20_auditd.py: RHELs 6.0, 6.1 have different checksums

--- valid/testing_modules/testcase_20_auditd.py
+++ valid/testing_modules/testcase_20_auditd.py
@@ -13,12 +13,18 @@ class testcase_20_auditd(ValidTestcase):
 
     def test(self, connection, params):
         if params['version'].startswith('6.'):
-            self.ping_pong(connection, 'md5sum /etc/audit/auditd.conf | cut -f 1 -d \' \'', 'e1886162554c18906df2ecd258aa4794')
-            self.ping_pong(connection, 'md5sum /etc/sysconfig/auditd  | cut -f 1 -d \' \'', 'd4d43637708e30418c30003e212f76fc')
+            if params['version'] in ['6.0', '6.1']:
+                auditd_checksum = '612ddf28c3916530d47ef56a1b1ed1ed'
+                auditd_sysconf_checksum = '123beb3a97a32d96eba4f11509e39da2'
+            else:
+                auditd_checksum = 'e1886162554c18906df2ecd258aa4794'
+                auditd_sysconf_checksum = 'd4d43637708e30418c30003e212f76fc'
             self.ping_pong(connection, 'chkconfig --list auditd', '3:on.*5:on')
         elif params['version'].startswith('5.'):
-            self.ping_pong(connection, 'md5sum /etc/audit/auditd.conf | cut -f 1 -d \' \'', '612ddf28c3916530d47ef56a1b1ed1ed')
-            self.ping_pong(connection, 'md5sum /etc/sysconfig/auditd  | cut -f 1 -d \' \'', '123beb3a97a32d96eba4f11509e39da2')
+            auditd_checksum = '612ddf28c3916530d47ef56a1b1ed1ed'
+            auditd_sysconf_checksum = '123beb3a97a32d96eba4f11509e39da2'
+        self.ping_pong(connection, 'md5sum /etc/audit/auditd.conf | cut -f 1 -d \' \'', auditd_checksum)
+        self.ping_pong(connection, 'md5sum /etc/sysconfig/auditd  | cut -f 1 -d \' \'', auditd_sysconf_checksum)
         self.ping_pong(connection, 'md5sum /etc/audit/audit.rules | cut -f 1 -d \' \'', 'f9869e1191838c461f5b9051c78a638d')
         self.ping_pong(connection, 'chkconfig --list auditd', '3:on.*5:on')
         return self.log
