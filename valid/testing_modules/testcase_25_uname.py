9ad58b266e009ce42b10bcbd69366e51ed4949dd
Vitaly Kuznetsov <vitty@redhat.com>
2013-09-18 13:16:50 +0200

    f20alpha

--- valid/testing_modules/testcase_25_uname.py
+++ valid/testing_modules/testcase_25_uname.py
@@ -26,7 +26,7 @@ class testcase_25_uname(ValidTestcase):
                 uname_r = self.get_result(connection, 'uname -r | sed -e \'s,\.i686$,,\'  -e \'s,\.x86_64$,,\'')
                 kernel_ver = self.get_result(connection, 'rpm -q --last kernel | sed -e \'s,^kernel-,,\' -e \'s,[[:space:]].*$,,\' | head -1 | sed -e \'s,\.i686$,,\'  -e \'s,\.x86_64$,,\'', 30)
         elif prod == 'FEDORA':
-            if ver == '18' and params['arch'] == 'i386':
+            if ver in ['18', '19', '20'] and params['arch'] == 'i386':
                 uname_r = self.get_result(connection, 'uname -r | sed \'s,\.PAE,,\'')
                 kernel_ver = self.get_result(connection, 'rpm -q --queryformat \'%{VERSION}-%{RELEASE}.%{ARCH}\n\' kernel-PAE | sort | tail -1', 30)
             else:
