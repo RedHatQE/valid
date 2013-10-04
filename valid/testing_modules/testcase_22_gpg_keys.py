7e846413ac131cba12029b50d0f8bbcdb6d8a90a
Ina Panova <ipanova@redhat.com>
2013-10-04 14:55:26 +0200

    testcase_22_gpg_keys.py:beta key should be present but not imported for rhel beta versions

--- valid/testing_modules/testcase_22_gpg_keys.py
+++ valid/testing_modules/testcase_22_gpg_keys.py
@@ -12,13 +12,12 @@ class testcase_22_gpg_keys(ValidTestcase):
 
     def test(self, connection, params):
         self.ping_pong(connection, 'grep \'^gpgcheck=\' /etc/yum.repos.d/redhat-*.repo | cut -d\= -f2 | sort -uf | tr -d \' \'', '\r\n1\r\n')
-        if params['product'].upper() == 'BETA':
-            self.ping_pong(connection, 'rpm -qa gpg-pubkey* | wc -l', '\r\n3\r\n', 10)
-        else:
-            self.ping_pong(connection, 'rpm -qa gpg-pubkey* | wc -l', '\r\n2\r\n', 10)
+        self.ping_pong(connection, 'rpm -qa gpg-pubkey* | wc -l', '\r\n2\r\n', 10)
         self.get_return_value(connection, 'rpm -q gpg-pubkey-2fa658e0-45700c69', 30)
         if params['version'].startswith('6.'):
             self.get_return_value(connection, 'rpm -q gpg-pubkey-fd431d51-4ae0493b', 30)
         elif params['version'].startswith('5.'):
             self.get_return_value(connection, 'rpm -q gpg-pubkey-37017186-45761324', 30)
+        if params['product'].upper() == 'BETA':
+            self.get_return_value(connection, 'test -f /etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-beta')        
         return self.log
