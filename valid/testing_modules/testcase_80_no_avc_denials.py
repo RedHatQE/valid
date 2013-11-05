d9fa1a33f01771a772aa13f3179d4f6c15e00bc7
Vitaly Kuznetsov <vitty@redhat.com>
2013-11-05 16:49:07 +0100

    log avc denials

--- valid/testing_modules/testcase_80_no_avc_denials.py
+++ valid/testing_modules/testcase_80_no_avc_denials.py
@@ -13,6 +13,6 @@ class testcase_80_no_avc_denials(ValidTestcase):
         prod = params['product'].upper()
         ver = params['version']
 
-        self.get_return_value(connection, 'grep \'avc:[[:space:]]*denied\' /var/log/messages /var/log/audit/audit.log', expected_status=1)
+        self.ping_pong(connection, 'echo START; grep \'avc:[[:space:]]*denied\' /var/log/messages /var/log/audit/audit.log; echo END', '\r\nSTART\r\nEND\r\n', 60)
 
         return self.log
