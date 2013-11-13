7491a8e20ea80eb46fec86fa7be215342ac326df
Ina Panova <ipanova@redhat.com>
2013-11-13 14:07:24 +0100

    testcase_03_chkconfig.py: rhel6.5 should have iptables disabled by default

--- valid/testing_modules/testcase_03_chkconfig.py
+++ valid/testing_modules/testcase_03_chkconfig.py
@@ -5,12 +5,11 @@ class testcase_03_chkconfig(ValidTestcase):
     """
     Check for several services runnung
     - crond
-    - iptables
+    - iptables( should be disabled by default in rhel6.5)
     - yum-updatesd (RHEL5 only)
     """
     stages = ['stage1']
     tags = ['default']
-
     def test(self, connection, params):
         is_systemd = self.get_result(connection, 'rpm -q systemd > /dev/null && echo True || echo False')
         if is_systemd == 'True':
@@ -18,7 +17,7 @@ class testcase_03_chkconfig(ValidTestcase):
             self.get_return_value(connection, 'systemctl is-active iptables.service')
         else:
             self.ping_pong(connection, 'chkconfig --list crond', '3:on')
-            self.ping_pong(connection, 'chkconfig --list iptables', '3:on')
+            self.ping_pong(connection, 'chkconfig --list iptables', '3:off' if params['version'] == '6.5' else '3:on')
             if (params['product'].upper() == 'RHEL' or params['product'].upper() == 'BETA') and params['version'].startswith('5.'):
                 self.ping_pong(connection, 'chkconfig --list yum-updatesd', '3:on')
         return self.log
