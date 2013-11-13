c60f00b2bf66f682aba8ebb5b7b148c82cd01a26
Ina Panova <ipanova@redhat.com>
2013-11-13 12:53:20 +0100

    testcase_37_sshd_bug923996.py: updated testcase for 6.5 version and later

--- valid/testing_modules/testcase_37_sshd_bug923996.py
+++ valid/testing_modules/testcase_37_sshd_bug923996.py
@@ -7,8 +7,11 @@ class testcase_37_sshd_bug923996(ValidTestcase):
     """
     stages = ['stage2']
     tags = ['default']
-    not_applicable = {'product': '(?i)RHEL|BETA', 'version': '^5\.[123456789]$|^6\.[123]$'}
+    not_applicable = {'product': '(?i)RHEL|BETA', 'version': '^5\.[123456789]$|^6\.[1234]$'}
 
     def test(self, connection, params):
-        self.get_return_value(connection, '[ `grep ^PermitRootLogin /etc/ssh/sshd_config | wc -l` -lt 2 ]')
+        if params['version'].startswith('5.'):
+            self.get_return_value(connection, '[ `grep ^PermitRootLogin /etc/ssh/sshd_config | wc -l` -lt 2 ]')
+        if params['version'].startswith('6.'):
+            self.get_return_value(connection, '[ `grep ^PermitRootLogin /etc/ssh/sshd_config | wc -l` -eq 0 ]')
         return self.log
