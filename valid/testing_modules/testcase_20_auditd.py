b773bf81b84d3bb38d63370d4d43c7a56476a8ed
Vitaly Kuznetsov <vitty@redhat.com>
2013-12-12 15:35:36 +0100

    check running auditd in running_services module

--- valid/testing_modules/testcase_20_auditd.py
+++ valid/testing_modules/testcase_20_auditd.py
@@ -19,12 +19,10 @@ class testcase_20_auditd(ValidTestcase):
             else:
                 auditd_checksum = 'e1886162554c18906df2ecd258aa4794'
                 auditd_sysconf_checksum = 'd4d43637708e30418c30003e212f76fc'
-            self.ping_pong(connection, 'chkconfig --list auditd', '3:on.*5:on')
         elif params['version'].startswith('5.'):
             auditd_checksum = '612ddf28c3916530d47ef56a1b1ed1ed'
             auditd_sysconf_checksum = '123beb3a97a32d96eba4f11509e39da2'
         self.ping_pong(connection, 'md5sum /etc/audit/auditd.conf | cut -f 1 -d \' \'', auditd_checksum)
         self.ping_pong(connection, 'md5sum /etc/sysconfig/auditd  | cut -f 1 -d \' \'', auditd_sysconf_checksum)
         self.ping_pong(connection, 'md5sum /etc/audit/audit.rules | cut -f 1 -d \' \'', 'f9869e1191838c461f5b9051c78a638d')
-        self.ping_pong(connection, 'chkconfig --list auditd', '3:on.*5:on')
         return self.log
