5afad33fef47527a2ce25aed79011619d332b0de
Vitaly Kuznetsov <vitty@redhat.com>
2014-04-30 10:18:58 +0200

    Revert "use get_return_value"
    This reverts commit 8a30e70d788ab5ac362db98bcbbb6c75f42b9b7b.

--- valid/testing_modules/testcase_80_no_avc_denials.py
+++ valid/testing_modules/testcase_80_no_avc_denials.py
@@ -13,6 +13,7 @@ class testcase_80_no_avc_denials(ValidTestcase):
     def test(self, connection, params):
         """ Perform test """
 
-        self.get_return_value(connection, 'cat /var/log/messages /var/log/audit/audit.log | grep -v userdata | grep \'avc:[[:space:]]*denied\'', 60, 1)
+        self.ping_pong(connection, 'echo START; grep \'avc:[[:space:]]*denied\' /var/log/messages /var/log/audit/audit.log | grep -v userdata; echo END',
+                       '\r\nSTART\r\nEND\r\n', 60)
 
         return self.log
