c2a407cc1b9e0e3290041be584fcbd7582e844fe
dparalen <vetrisko@gmail.com>
2014-05-26 18:40:25 +0200

    fix: rhel7 has xfs but no vfs

--- valid/testing_modules/testcase_32_ephemeral.py
+++ valid/testing_modules/testcase_32_ephemeral.py
@@ -1,5 +1,6 @@
 """ This module contains testcase_32_ephemeral test """
 from valid.valid_testcase import ValidTestcase
+from os.path import basename
 
 
 class testcase_32_ephemeral(ValidTestcase):
@@ -15,35 +16,48 @@ class testcase_32_ephemeral(ValidTestcase):
 
         prod = params['product'].upper()
         ver = params['version'].upper()
-        has_ephemeral = False
-        for bdev in params['bmap']:
-            if 'ephemeral_name' in bdev.keys():
-                name = bdev['name']
-                if (prod in ['RHEL', 'BETA']) and (ver.startswith('5.')):
-                    if name.startswith('/dev/xvd'):
-                        # no xvd* for RHEL5
-                        continue
-                elif (prod in ['RHEL', 'BETA']) and (ver.startswith('6.') and ver != '6.0'):
-                    if name.startswith('/dev/sd'):
-                        name = '/dev/xvd' + name[7:]
-                    if params['virtualization'] != 'hvm' and ver != '6.5' and len(name) == 9 and ord(name[8]) < ord('w'):
-                        # there is a 4-letter shift
-                        name = name[:8] + chr(ord(name[8]) + 4)
-                else:
-                    # Fedora and newer RHELs
-                    if name.startswith('/dev/sd'):
-                        name = '/dev/xvd' + name[7:]
-                has_ephemeral = True
-                self.get_return_value(connection, 'fdisk -l %s | grep \'^Disk\'' % name, 30)
-                if self.get_result(connection, 'grep \'%s \' /proc/mounts  | wc -l' % name, 5) == '0':
-                    # device is not mounted, doing fs creation
-                    if self.get_result(connection, 'ls -la /sbin/mkfs.vfat 2> /dev/null | wc -l', 5) == '1':
-                        # mkfs.vfat is faster!
-                        self.get_return_value(connection, 'mkfs.vfat -I %s' % name, 60)
-                    else:
-                        self.get_return_value(connection, 'mkfs.ext3 %s' % name, 300)
-        if not has_ephemeral:
+        ephemerals = []
+
+        if 'bmap' in params:
+            ephemerals = [bdev for bdev in params['bmap'] if 'ephemeral_name' in bdev]
+
+        if not ephemerals:
+            # are there are some devices to check?
             self.log.append({'result': 'skip',
                              'comment': 'no ephemeral devices in block map'
                              })
+            return self.log
+
+        # figure out what fstype to use with focus on mkfs.<fstype> speed
+        for fstype in ['vfat', 'xfs', 'ext3']:
+            if self.get_result(connection, 'ls -la /sbin/mkfs.%s 2> /dev/null | wc -l' % fstype, 5) == '1':
+                break
+
+        for bdev in ephemerals:
+            name = bdev['name']
+            if (prod in ['RHEL', 'BETA']) and (ver.startswith('5.')):
+                if name.startswith('/dev/xvd'):
+                    # no xvd* for RHEL5
+                    continue
+            elif (prod in ['RHEL', 'BETA']) and (ver.startswith('6.') and ver != '6.0'):
+                if name.startswith('/dev/sd'):
+                    name = '/dev/xvd' + name[7:]
+                if params['virtualization'] != 'hvm' and ver != '6.5' and len(name) == 9 and ord(name[8]) < ord('w'):
+                    # there is a 4-letter shift
+                    name = name[:8] + chr(ord(name[8]) + 4)
+            else:
+                # Fedora and newer RHELs
+                if name.startswith('/dev/sd'):
+                    name = '/dev/xvd' + name[7:]
+            # test: check device presence
+            self.get_return_value(connection, 'fdisk -l %s | grep \'^Disk\'' % name, 30)
+            if self.get_result(connection, 'grep \'%s \' /proc/mounts  | wc -l' % name, 5) == '0':
+                # device is not mounted, doing fs creation
+                # test: mkfs
+                self.get_return_value(connection, 'mkfs.%s %s' % (fstype, name), 1000)
+                # create mount-point
+                if self.get_result(connection, 'mkdir -p /tmp/mnt-%s' % basename(name), 5) == '0':
+                    # test: mount
+                    self.get_return_value(connection, 'mount -t %s %s /tmp/mnt-%s' % (fstype, name, basename(name)), 60)
+
         return self.log
