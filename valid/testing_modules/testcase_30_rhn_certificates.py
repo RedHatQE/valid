cd9e85ed18ca58fd19fcefc388d2799d1053a894
dparalen <vetrisko@gmail.com>
2014-07-16 14:59:03 +0200

    fix: avoid egrep --- colour escape sequences creeped in; run on RHEL7; remove exp_warn_time failing

--- valid/testing_modules/testcase_30_rhn_certificates.py
+++ valid/testing_modules/testcase_30_rhn_certificates.py
@@ -1,8 +1,8 @@
 """ This module contains testcase_30_rhn_certificates test """
 from valid.valid_testcase import ValidTestcase
-from datetime import datetime, timedelta
+from datetime import datetime
 from distutils.version import LooseVersion as Version
-EXP_WARN_TIME = timedelta(90) # fail the check if the cert is less than 90 days to expiration
+import re
 
 
 def _expiration_date(params):
@@ -15,12 +15,14 @@ def _expiration_date(params):
         expiration = 7
     else:
         expiration = 10
-    if params['version'].startswith('5.'):
-        return datetime(2007 + expiration, 3, 14) + EXP_WARN_TIME
-    elif params['version'].startswith('6.'):
-        return datetime(2010 + expiration, 11, 10) + EXP_WARN_TIME
-    else:
-        raise ValueError('release %s unsupported' % params['version'])
+    version = Version(params['version'])
+    if version < '6.0':
+        return datetime(2007 + expiration, 3, 14)
+    if version < '7.0':
+        return datetime(2010 + expiration, 11, 10)
+    if version < '8.0':
+        return datetime(2014 + expiration, 6, 10)
+    raise ValueError('release %s unsupported' % params['version'])
 
 
 class testcase_30_rhn_certificates(ValidTestcase):
@@ -38,10 +40,8 @@ class testcase_30_rhn_certificates(ValidTestcase):
             config_rpms = 'rh-amazon-rhui-client rh-amazon-rhui-client-beta'
         else:
             config_rpms = 'rh-amazon-rhui-client'
-        cert_files = self.get_result(
-            connection,
-            r"rpm -ql %s | egrep '.*\.(pem|crt)'" % config_rpms
-        )
+        cert_files = [cert_file for cert_file in self.get_result(connection, "rpm -ql " + config_rpms).split() \
+                        if re.match(r'.*\.(crt|pem)$', cert_file)]
         # for each cert file, the notAfter field is examined
         # against the expiration_date and the result is stored in self.log
         try:
@@ -50,11 +50,15 @@ class testcase_30_rhn_certificates(ValidTestcase):
             # just log and return in case expiration can't be determined
             self.log.append({'result': 'failure', 'comment': str(err)})
             return self.log
-        for cert in cert_files.split():
-            date_string = self.get_result(
-                connection,
-                'openssl x509 -in %s -noout -dates | grep notAfter' % cert
-            )
+        for cert in cert_files:
+            # the -dates format is:
+            # notBefore=Jan 17 10:29:59 2013 GMT
+            # notAfter=Jan 17 10:29:59 2014 GMT
+            # just the notAfter line is required
+            date_string = re.split(
+                r'\r?\n',
+                self.get_result(connection, 'openssl x509 -in %s -noout -dates' % cert).strip()
+            )[-1]
             if date_string and date_string.find('=') != -1:
                 cert_expiration = datetime.strptime(
                     # the date_string has the form:
